552db836016b79de24efdf0db53b88faace73629
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-09-23 22:54:32 +0200

    fix: Add new links to mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon, SunIcon, MoonIcon } from "@heroicons/react/24/outline";
@@ -64,14 +63,14 @@ export function Navbar() {
                     <div className="fixed inset-0 z-50" />
                     <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto border-l-4 border-emerald-500 bg-slate-100 p-6 dark:bg-neutral-900 sm:max-w-sm">
                         <div className="flex justify-between">
-                            <a href="#" className="-m-1.5 p-1.5">
+                            <HashLink to="/" elementId="top" className="-m-1.5 p-1.5">
                                 <span className="sr-only">Logo</span>
                                 <img
                                     className="h-12 w-auto invert dark:invert-0"
                                     src={logo}
                                     alt="Logo"
                                 />
-                            </a>
+                            </HashLink>
                             <button
                                 type="button"
                                 className="-m-2.5 p-2 text-gray-700 dark:text-slate-200"
@@ -88,16 +87,17 @@ export function Navbar() {
                             <div className="-my-6">
                                 <div className="space-y-2 py-6">
                                     {navigation.map((item) => (
-                                        <Link
+                                        <HashLink
                                             key={item.name}
                                             to={item.href}
+                                            elementId={item.scrollTo}
                                             className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-slate-200 dark:text-slate-200 dark:hover:bg-neutral-800"
                                             onClick={() =>
                                                 setMobileMenuOpen(false)
                                             }
                                         >
                                             {item.name}
-                                        </Link>
+                                        </HashLink>
                                     ))}
                                 </div>
                             </div>
