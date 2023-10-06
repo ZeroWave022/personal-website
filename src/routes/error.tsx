4fa9b0208041f4fe3b49b3c77fd013c84d203a16
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-10-06 19:43:02 +0200

    feat: Translate error page

--- src/routes/error.tsx
+++ src/routes/error.tsx
@@ -1,8 +1,11 @@
 import { useRouteError } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
 import { DarkModeToggle } from "../components/DarkModeToggle";
 import { LargeLink } from "../components/LargeLink";
 
 export default function Error() {
+    const { t } = useTranslation();
     const error = useRouteError() as { statusText?: string; message?: string };
     console.error(error);
 
@@ -13,15 +16,15 @@ export default function Error() {
             </div>
 
             <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-slate-100 dark:bg-neutral-900 dark:text-slate-200">
-                <h1>Sorry!</h1>
+                <h1>{t("error.header")}</h1>
                 <div>
-                    <h2>An unexpected error has occured</h2>
+                    <h2>{t("error.subheader")}</h2>
                     <span className="block text-center text-xl font-light text-gray-500 dark:text-slate-300">
                         {error.statusText ?? error.message}
                     </span>
                 </div>
                 <LargeLink color="green-light" link="/" internal={true}>
-                    Go back to Home
+                    {t("error.goBack")}
                 </LargeLink>
             </main>
         </>
